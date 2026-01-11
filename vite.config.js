5bf64963d07a414f905361d2fdadb74f449ad919
Big_Head_YK <yoking0626@gmail.com>
2026-01-12 03:09:03 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    // 開發中、產品路徑
+    base:
+        process.env.NODE_ENV === "production"
+            ? "/React-ZDX-Club-Booking/"
+            : "/",
+    plugins: [react()],
+});
